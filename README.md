42195e88fc3996f3ebe19a3b1a8c9d9538badd73
堀 哲也 <axis38akasira@gmail.com>
2017-05-26 19:29:52 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # mosaicDesigner
-A program for making a bitmap (mosaic )image.
+A program for making a square bitmap (mosaic) image.
+
+## How it works?
+This program make an image from two texts data named design.txt and pattern.txt  
+
+The file "design.txt" indicates design.    
+Format:  
+16								\# size (width / height)  
+2121212121212121	\# 1 means color1  
+1212121212121212	\# 2 means color2  
+2121212121212121	\# (n) means color(n)  
+1212121212121212	\# (max 15 colors + background)  
+2121212121212121	  
+1212121212121212	  
+2121212121212121	  
+1212121212121212	  
+2121212121212121	  
+1212121212121212	  
+2121212121212121	  
+1212121212121212	  
+2121212121212121	  
+1212121212121212	  
+2121212121212121	  
+1212121212121212	  
+  
+You can also write like this.  
+2  
+21  
+12  
+If you choose small data, it will be expanded automatically.
+  
+The file "pattern.txt" indicates the shape of the image.  
+16  								\# size (width / height)  
+0000000000000000	\# 0 means background  
+0000000010000000	\# 1 means color  
+0000010010010000  
+0000100100100000  
+0001001001000000  
+0001001001000000  
+0001001001000000  
+0000100100100000  
+0010010010010100  
+0100010010010010  
+1000010010010001  
+1000100100100001  
+1000000100000001  
+0110000000000110  
+0011000000001100  
+0000111111110000  
+  
---
+++ main.c
@@ -0,0 +1,208 @@
+//
+//  main.c
+//  mosaicDesigner
+//
+//
+//  Created by 堀 哲也 on 2017/04/03.
+//  Copyright © 2017年 堀 哲也. All rights reserved.
+//
+//  プロフィール画像に使用できるような正方形のbmp画像(4bit, OS/2)を出力する
+//  (カラーパレット使用で画像データ自体は小さくする)
+//
+//  次の規定に沿った小さな正方形を敷き詰めて画像を得る
+//  1. 模様データを記録してあるdesign.txtを読み込み、配列に展開する
+//      (背景を0、アクセントカラー1を1, アクセントカラー2を2として記述)
+//  2. シルエットの形を記録してあるpattern.txtを読み込み、配列に展開する
+//      (背景を0、シルエットを1として文字で記述されるパターン)
+//  3. 1の模様データを2のパターンでマスクする(patternが0なら0を、1ならdesignの値を書き込む)
+//  (4. 画像の周囲を背景色で縁取る)
+//
+
+#include <stdio.h>
+
+#define FILEHEADER 14   // ファイルヘッダの大きさ
+#define INFOHEADER 12   // 情報ヘッダの大きさ
+#define SQUARESIZE 32   // 小さな正方形の一辺の長さ
+#define FLAMETHICK 1    // 縁取りの線の厚さ
+
+#define GRIDSIZE 512    // 配列の数
+
+// この構造体は模様とパターンで兼用
+typedef struct _pic{
+    char grid[GRIDSIZE][GRIDSIZE];
+    //FILE *fp;
+    int length; // 一辺の長さ
+} PIC;
+
+int input(PIC *target, const char *name);
+void adjust(PIC *design, int length);
+void mask(PIC design, PIC pattern, PIC *result);
+int output();
+
+int main() {
+    /* 変数宣言 */
+    PIC design, pattern, result;
+    
+    /* パターン入力 */
+    if(input(&design, "design.txt"))
+        return 1;   // end if file can't be opened
+    if(input(&pattern, "pattern.txt"))
+        return 1;
+    
+    // fixme
+    result.length = pattern.length; // 出力の画像サイズはパターンと同じにする
+    
+    /* データ処理 */
+    adjust(&design, pattern.length);
+    mask(design, pattern, &result);
+    
+    /* 出力 */
+    if(output(result)){
+        return 1;   // end if file can't be opened
+    }
+    
+    /* 終了 */
+    return 0;
+}
+
+int input(PIC *target, const char *name){
+    char buff;
+    int i=0, j=0;   // column, row
+    FILE *fp;
+    
+    if((fp = fopen(name, "r")) == NULL){
+        printf("file %s can't be opened\n", name);
+        return 1;
+    }
+    
+    fscanf(fp, "%d", &target->length);
+    
+    for(i = 0; i < target->length; i++){
+        for(j = 0; j < target->length; j++){
+            fscanf(fp, " %c", &buff);
+            target->grid[i][j] = buff - '0';
+        }
+    }
+    
+    fclose(fp);
+    return 0;
+}
+
+// 模様データがパターンデータより小さい場合は、縦横に繰り返す
+void adjust(PIC *design, int patternLen){
+    char tempGrid[GRIDSIZE][GRIDSIZE];    // 元々の模様データを退避しておく
+    int originalLen = design->length;
+    int i, j;
+    
+    for(i = 0; i < design->length; i++)
+        for(j = 0; j < design->length; j++)
+            tempGrid[i][j] = design->grid[i][j];
+    
+    for(i = 0; i < patternLen; i++){
+        for(j = 0; j < patternLen; j++){
+            design->grid[i][j] = tempGrid[i % originalLen][j % originalLen];    // fixme
+        }
+    }
+}
+
+void mask(PIC design, PIC pattern, PIC *result){
+    int i, j;
+    
+    for(i = 0; i < pattern.length; i++){
+        for(j = 0; j < pattern.length; j++){
+            if(pattern.grid[i][j] == 0)
+                result->grid[i][j] = 0; // patternが0ならresultも0
+            else
+                result->grid[i][j] = design.grid[i][j]; // patternが1ならresultはdesignに従う
+        }
+    }
+}
+
+int output(PIC picture){
+    // ------------------ 画像内容パラメータ -------------------
+    const int color[] = {0xFFF3FD, 0xDBD0E6, 0xFDD3FF}; // 背景色、アクセントカラー1,2の順 16進数で
+    //----------------再定義---------------------------
+    FILE *fp;
+    int length = picture.length;
+    //----------------変数--------------------------------
+    int picLen = SQUARESIZE * length;  // 画像の縦及び横の長さ
+    unsigned long buff;
+    //------------------ 制御用 -----------------------------
+    int i, j, k, l;
+    //---------------------------------------------------------
+    
+    fp = fopen("output.bmp", "wb");
+    if(fp == NULL){
+        printf("file opening process failed\n");
+        return 1;
+    }
+    
+    // ファイルヘッダ
+    fwrite("BM", 2, 1, fp);     // bmpであることを示す
+    
+    // ファイルサイズ
+    buff = FILEHEADER + INFOHEADER + 3 * 16 + picLen * picLen / 2;
+    fwrite(&buff, 4, 1, fp);    // ファイルヘッダ+情報ヘッダ+パレット+画像データ
+    
+    buff = 0;
+    fwrite(&buff, 4, 1, fp);    // 予約領域
+    
+    buff = FILEHEADER + INFOHEADER + 3 * 16;    // 先頭から画像データまでのオフセット
+    fwrite(&buff, 4, 1, fp);    // ファイルヘッダ + 情報ヘッダ + パレットデータ
+    
+    // 情報ヘッダ
+    buff = INFOHEADER;
+    fwrite(&buff, 4, 1, fp);    // 情報ヘッダの大きさ
+    
+    buff = picLen;              // 縦横の長さ
+    fwrite(&buff, 2, 1, fp);    // 正方形なので同じ数値を2つ
+    fwrite(&buff, 2, 1, fp);
+    
+    buff = 1;
+    fwrite(&buff, 2, 1, fp);    // bcプレーン数（？）は常に1
+    
+    buff = 4;
+    fwrite(&buff, 2, 1, fp);    // 4bitの画像である
+    
+    // パレットデータ
+    for(i = 0; i < 3; i++){  // 最初の3色にcolor[3]のデータを入れ、残りは0に設定
+        buff = 0;   // buffのリセット
+        // BGRの順に設定する
+        buff = color[i] & 0x0000FF;
+        fwrite(&buff, 1, 1, fp);
+        
+        buff = color[i] & 0x00FF00;
+        buff = buff >> 8;
+        fwrite(&buff, 1, 1, fp);
+        
+        buff = color[i] & 0xFF0000;
+        buff = buff >> 16;
+        fwrite(&buff, 1, 1, fp);
+    }
+    
+    for(i = 0; i < 13; i++){
+        buff = 0;
+        fwrite(&buff, 3, 1, fp);
+    }
+    
+    // 画像データ
+    // 1Byteに2pixelのデータが入ることに注意
+    // 画像データは左下から右上に記録される
+    for(i = 0; i < length; i++){                    // 縦のパターンの数だけループ
+        for(j = 0; j < SQUARESIZE; j++){            // 小さい正方形の縦の長さだけループ
+            for(k = 0; k < length; k++){            // 横のパターンの数だけループ
+                for(l = 0; l < SQUARESIZE/2; l++){  // 小さい正方形の横の長さ/2だけループ
+                    buff = (picture.grid[length-i-1][k] << 4) + picture.grid[length-i-1][k];  // 4bitごとに色をセット
+                    if(j < FLAMETHICK || j >= SQUARESIZE-FLAMETHICK || l < FLAMETHICK || l >= SQUARESIZE-FLAMETHICK){
+                        // 縁取りの対象なら
+                        buff = 0b00000000;
+                    }
+                    fwrite(&buff, 1, 1, fp);
+                }
+            }
+        }
+    }
+    
+    fclose(fp);
+    return 0;
+}
